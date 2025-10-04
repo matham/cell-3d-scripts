0116bd938e05e9458e59ace4449ee3c89b941050
Matt Einhorn <matt@einhorn.dev>
2025-10-04 00:35:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# cell-3d-scripts
+
+[![License MIT](https://img.shields.io/pypi/l/cell-3d-scripts.svg?color=green)](https://github.com/matham/cell-3d-scripts/raw/main/LICENSE)
+[![PyPI](https://img.shields.io/pypi/v/cell-3d-scripts.svg?color=green)](https://pypi.org/project/cell-3d-scripts)
+[![Python Version](https://img.shields.io/pypi/pyversions/cell-3d-scripts.svg?color=green)](https://python.org)
+[![tests](https://github.com/matham/cell-3d-scripts/workflows/tests/badge.svg)](https://github.com/matham/cell-3d-scripts/actions)
+[![codecov](https://codecov.io/gh/matham/cell-3d-scripts/branch/main/graph/badge.svg)](https://codecov.io/gh/matham/cell-3d-scripts)
+
+
+
+----------------------------------
+
+## Installation
+
+You can install `cell-3d-scripts` via [pip]:
+
+    pip install cell-3d-scripts
+
+
+
+To install latest development version :
+
+    pip install git+https://github.com/matham/cell-3d-scripts.git
+
+
+## License
+
+Distributed under the terms of the [MIT] license,
+"cell-3d-scripts" is free and open source software
---
+++ cell_3d_scripts/locate_cells.py
@@ -0,0 +1,215 @@
+import logging
+import multiprocessing as mp
+import tempfile
+import zipfile
+from argparse import (
+    ArgumentDefaultsHelpFormatter,
+    ArgumentParser,
+)
+from copy import deepcopy
+from datetime import datetime
+from functools import partial
+from pathlib import Path
+
+import fancylog
+import numpy as np
+import tqdm
+import zarr
+from brainglobe_utils.cells.cells import Cell
+from brainglobe_utils.IO.cells import get_cells, save_cells
+from brainglobe_utils.IO.image import read_z_stack
+
+import cell_3d_scripts
+from cell_3d_scripts import __version__
+
+
+def _scale_point(
+    point: tuple[float, float, float], input_size: tuple[int, int, int], output_size: tuple[int, int, int]
+) -> tuple[int, int, int]:
+    output = []
+    for p, i, o in zip(point, input_size, output_size, strict=False):
+        # last value is i - 1
+        v = p / (i - 1) * (o - 1)
+        v = int(round(v))
+        output.append(v)
+
+    return tuple(output)
+
+
+def arg_parser() -> ArgumentParser:
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        "-s",
+        "--signal-planes-path",
+        dest="signal_planes_path",
+        type=str,
+        default="",
+        required=False,
+        help="Path to the directory of the signal files. Can also be a text"
+        "file pointing to the files. For a 3d tiff, data is in z, y, x order",
+    )
+    parser.add_argument(
+        "--data-size",
+        dest="signal_data_size",
+        type=int,
+        nargs=3,
+        default=(),
+        required=False,
+    )
+    parser.add_argument(
+        "-c",
+        "--cells-path",
+        dest="cells_path",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-r",
+        "--region-id-path",
+        dest="region_id_path",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--atlas-name",
+        dest="atlas_name",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--output-cells-path",
+        dest="output_cells_path",
+        type=str,
+        required=False,
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
+    )
+    parser.add_argument(
+        "--workers",
+        dest="num_workers",
+        type=int,
+        required=False,
+        default=6,
+    )
+
+    return parser
+
+
+def _look_up_cell(arg, region_ids: np.ndarray) -> tuple[int, int]:
+    cell_i, pos = arg
+    return cell_i, region_ids[pos].item()
+
+
+def main(
+    *,
+    data_size_voxels: tuple[int, int, int],
+    cells: list[Cell],
+    region_ids: np.ndarray,
+    atlas_name: str,
+    output_cells_path: Path | None = None,
+    num_workers: int = 6,
+) -> list[Cell]:
+    ts = datetime.now()
+    regions_voxels = region_ids.shape
+    logging.info(f"cell_meta_3d: Starting region ID lookup for atlas {atlas_name} for {len(cells)} cells")
+    logging.debug(f"Region IDs shape is {regions_voxels}. Data shape is {data_size_voxels}")
+
+    cells = sorted(cells, key=lambda c: (c.z, c.x, c.y))
+    output_cells = deepcopy(cells)
+
+    if num_workers:
+        cell_pos = [_scale_point((cell.z, cell.y, cell.x), data_size_voxels, regions_voxels) for cell in output_cells]
+
+        progress_bar = tqdm.tqdm(total=len(output_cells), unit="cells")
+        f = partial(_look_up_cell, region_ids=region_ids)
+        ctx = mp.get_context("spawn")
+        # we can't use the context manager because of coverage issues:
+        pool = ctx.Pool(processes=num_workers)
+        try:
+            for cell_i, region_id in pool.imap_unordered(f, list(enumerate(cell_pos))):
+                output_cells[cell_i].metadata[f"region_id_{atlas_name}"] = region_id
+                progress_bar.update()
+        finally:
+            pool.close()
+            pool.join()
+        progress_bar.close()
+    else:
+        for cell in tqdm.tqdm(output_cells, unit="cells"):
+            pos = _scale_point((cell.z, cell.y, cell.x), data_size_voxels, regions_voxels)
+            cell.metadata[f"region_id_{atlas_name}"] = region_ids[pos].item()
+
+    if output_cells_path:
+        save_cells(output_cells, str(output_cells_path))
+    logging.info(f"cell_meta_3d: Analysis took {datetime.now() - ts}")
+
+    return output_cells
+
+
+def run_main():
+    args = arg_parser().parse_args()
+
+    output_cells_path = Path(args.cells_path)
+    output_root = Path(args.cells_path).parent
+    if args.output_cells_path:
+        output_cells_path = Path(args.output_cells_path)
+        output_cells_path.parent.mkdir(parents=True, exist_ok=True)
+        output_root = output_cells_path.parent
+
+    fancylog.start_logging(
+        output_root,
+        cell_3d_scripts,
+        variables=[
+            args,
+        ],
+        log_header="Cell3DScripts:LocateCells Log",
+        multiprocessing_aware=True,
+    )
+
+    if args.signal_data_size:
+        data_size_voxels = args.signal_data_size
+    elif args.signal_planes_path:
+        data_size_voxels = read_z_stack(args.signal_planes_path).shape
+    else:
+        raise ValueError("Either --signal-planes-path or --data-size must be provided")
+
+    logging.debug(f"Loading cells from {args.cells_path}")
+    cells = get_cells(args.cells_path, cells_only=True)
+
+    region_id_path = Path(args.region_id_path)
+    atlas_name = args.atlas_name
+
+    logging.debug(f"Using region IDs from {region_id_path}")
+    tempdir = None
+    if region_id_path.suffix == ".zarr":
+        group = zarr.open(region_id_path, mode="r")
+        arr = group.get("data")
+    elif region_id_path.name.endswith(".zarr.zip"):
+        tempdir = tempfile.TemporaryDirectory(ignore_cleanup_errors=True)
+        with zipfile.ZipFile(region_id_path, "r") as zf:
+            zf.extractall(tempdir.name)
+        group = zarr.open(tempdir.name, mode="r")
+        arr = group.get("data")
+    else:
+        arr = read_z_stack(str(region_id_path))
+
+    try:
+        main(
+            data_size_voxels=data_size_voxels,
+            cells=cells,
+            region_ids=arr,
+            atlas_name=atlas_name,
+            output_cells_path=output_cells_path,
+            num_workers=args.num_workers,
+        )
+    finally:
+        if tempdir is not None:
+            tempdir.cleanup()
+
+
+if __name__ == "__main__":
+    run_main()
